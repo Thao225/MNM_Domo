bc7e7f9bd592f4c0cd494658e1753e3985dc454b
Thao225 <buithienhuongthao2005@gmail.com>
2024-10-31 14:35:29 +0700

    up

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://MNM/todo-app", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://hostname/todo-app", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
